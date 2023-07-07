765e9f4f436879cab20a12ef745c85746b0f9f14
ldev <ldev@ldev.eu.org>
2023-07-07 18:35:41 +0000

    added mic volume

--- blocks.h
+++ blocks.h
@@ -2,9 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"\uf028 ", "pactl get-sink-volume @DEFAULT_SINK@ | awk '/Volume/ {print $5}'",         30,		1},
+	{"\uf130", "pactl get-source-volume @DEFAULT_SOURCE@ | awk '/Volume/ {print $5}'",      30,		1},
 	{"\uf4bc ", "mpstat | awk '/all/ { print int(100-$13) \"%\"}'",                         5,		0},
 	{"\ue266 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	                10,		0},
-	{"\uf455 ", "date '+%b %d (%a) | \uf43a %I:%M%p'",                                       5,     0},
+	{"\uf455 ", "date '+%b %d (%a) | \uf43a %I:%M%p'",                                      5,      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
